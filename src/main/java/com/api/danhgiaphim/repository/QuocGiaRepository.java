e44b4d62e48c4a4aeac1bfcb1c1b2b149bbaa6b2
Dinh Tran Trong Hieu <dinhtrantronghieu2004@gmail.com>
2025-06-11 14:15:43 +0700

    HAHA

--- src/main/java/com/api/danhgiaphim/repository/QuocGiaRepository.java
+++ src/main/java/com/api/danhgiaphim/repository/QuocGiaRepository.java
@@ -1,10 +1,10 @@
 package com.api.danhgiaphim.repository;
 
-import com.api.danhgiaphim.entity.QuocGiaEntity;
+import com.api.danhgiaphim.entity.QuocGia;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface QuocGiaRepository extends JpaRepository<QuocGiaEntity, Integer>{
+public interface QuocGiaRepository extends JpaRepository<QuocGia, Integer>{
     
 }
--- src/main/java/com/api/danhgiaphim/service/QuocGiaService.java
+++ src/main/java/com/api/danhgiaphim/service/QuocGiaService.java
@@ -1,20 +1,46 @@
 package com.api.danhgiaphim.service;
 
 import com.api.danhgiaphim.dto.request.QuocGiaCreationRequest;
-import com.api.danhgiaphim.entity.QuocGiaEntity;
+import com.api.danhgiaphim.entity.QuocGia;
 import com.api.danhgiaphim.repository.QuocGiaRepository;
+import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 @Service
 public class QuocGiaService {
+
     @Autowired
     private QuocGiaRepository quocGiaRepository;
-    
-    public QuocGiaEntity createQuocGia(QuocGiaCreationRequest request){
-        QuocGiaEntity quocGiaEntity = new QuocGiaEntity();
+
+    public QuocGia createQuocGia(QuocGiaCreationRequest request) {
+        QuocGia quocGiaEntity = new QuocGia();
         quocGiaEntity.setTenQuocGia(request.getTenQuocGia());
-        
+
         return quocGiaRepository.save(quocGiaEntity);
     }
+
+    public List<QuocGia> saveAllQuocGias(List<QuocGia> quocGias) {
+        return quocGiaRepository.saveAll(quocGias);
+    }
+
+    public List<QuocGia> getQuocGias() {
+        return quocGiaRepository.findAll();
+    }
+    
+    public QuocGia getQuocGia(Integer id){
+        return quocGiaRepository.findById(id).orElseThrow(() -> new RuntimeException("Không tìm thấy quốc gia"));
+    }
+    
+    public QuocGia updateQuocGia(Integer id,QuocGiaCreationRequest request){
+        QuocGia quocGia = getQuocGia(id);
+        quocGia.setTenQuocGia(request.getTenQuocGia());
+        
+        return quocGiaRepository.save(quocGia);
+    }
+    
+    public void deleteQuocGia(Integer id){
+        quocGiaRepository.deleteById(id);
+    }
+    
 }
