8fd004f89fbebf052a0cdb59d3072e53c940864d
Dinh Tran Trong Hieu <dinhtrantronghieu2004@gmail.com>
2025-06-23 10:53:50 +0700

    Xong API dien vien

--- src/main/java/com/api/danhgiaphim/Controller/DienVienController.java
+++ src/main/java/com/api/danhgiaphim/Controller/DienVienController.java
@@ -1,21 +1,49 @@
 package com.api.danhgiaphim.Controller;
 
+import com.api.danhgiaphim.dto.request.DienVienRequest;
 import com.api.danhgiaphim.entity.DienVien;
 import com.api.danhgiaphim.service.DienVienService;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/dienviens")
 public class DienVienController {
+
     @Autowired
     private DienVienService dienVienService;
 
     @GetMapping
     public List<DienVien> selectDienVien() {
         return dienVienService.getDienViens();
+    }
+
+    @PostMapping
+    DienVien createDienVien(@RequestBody DienVienRequest request) {
+        return dienVienService.createDienVien(request);
+    }
+
+    @GetMapping("/{maDienVien}")
+    DienVien selectDienVienById(@PathVariable("maDienVien") Integer maDienVien) {
+        return dienVienService.getDienVien(maDienVien);
+    }
+
+    @PutMapping("/{maDienVien}")
+    DienVien updateDienVien(@PathVariable("maDienVien") Integer maDienVien, @RequestBody DienVienRequest request) {
+        return dienVienService.updateDienVien(maDienVien, request);
+    }
+
+    @DeleteMapping("/{maDienVien}")
+    String xoaDienVien(@PathVariable("maDienVien") Integer maDienVien) {
+        dienVienService.deleteDienVien(maDienVien);
+        return "Đã Xoá Diễn Viên";
     }
 }
--- src/main/java/com/api/danhgiaphim/service/DienVienService.java
+++ src/main/java/com/api/danhgiaphim/service/DienVienService.java
@@ -1,5 +1,6 @@
 package com.api.danhgiaphim.service;
 
+import com.api.danhgiaphim.dto.request.DienVienRequest;
 import com.api.danhgiaphim.entity.DienVien;
 import com.api.danhgiaphim.repository.DienVienRepository;
 import java.util.List;
@@ -14,5 +15,33 @@ public class DienVienService {
 
     public List<DienVien> getDienViens() {
         return dienVienRepository.findAll();
+    }
+
+    public DienVien getDienVien(Integer id) {
+        return dienVienRepository.findById(id).orElseThrow(() -> new RuntimeException("Không tìm thấy diễn viên"));
+    }
+
+    public DienVien createDienVien(DienVienRequest request) {
+        DienVien dienVien = new DienVien();
+        dienVien.setTenDienVien(request.getTenDienVien());
+        dienVien.setAnhDienVien(request.getAnhDienVien());
+        dienVien.setNgaySinh(request.getNgaySinh());
+        dienVien.setQuocGia(request.getQuocGia());
+
+        return dienVienRepository.save(dienVien);
+    }
+
+    public DienVien updateDienVien(Integer id, DienVienRequest request) {
+        DienVien dienVien = getDienVien(id);
+        dienVien.setTenDienVien(request.getTenDienVien());
+        dienVien.setAnhDienVien(request.getAnhDienVien());
+        dienVien.setNgaySinh(request.getNgaySinh());
+        dienVien.setQuocGia(request.getQuocGia());
+
+        return dienVienRepository.save(dienVien);
+    }
+
+    public void deleteDienVien(Integer id) {
+        dienVienRepository.deleteById(id);
     }
 }
